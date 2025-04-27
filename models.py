d361184ccdfffa60d33c4cc45c0fd4975f44c2db
CyberNet Labs Inc. <rupture_general_0a@icloud.com>
2025-04-27 19:09:32 +0200

    Add SQLAlchemy Project model

---
+++ models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=True)
+
+    def __repr__(self):
+        return f'<Project {self.name}>'
